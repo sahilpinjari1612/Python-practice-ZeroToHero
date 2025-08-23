0f72221a6f9d4ab64dd9043b3aff00376054e8d6
sahilpinjari1612 <sahilpinjari1612@gmail.com>
2025-08-23 12:32:49 +0530

    Day 1: Basics - Hello World, Variables, Data Types

---
+++ Day1/Day1.md
@@ -0,0 +1,13 @@
+# Day 1 - Python Basics
+
+## Topics Covered
+- Printing in Python
+- Variables
+- Data Types (string, int, float, bool)
+- type() function
+
+## Example
+```python
+name = "Sahil"
+age = 20
+print(name, age)
---
+++ Day1/day1_basics.py
@@ -0,0 +1,25 @@
+# Day 1 - Python Basics
+
+# Task 1:Hello World
+print("Hello, World!")
+
+# Task 2: Variables & Data Types
+
+# Variables
+name = "Sahil Pinjari"
+age = 20
+weight = 72.5
+is_student = True
+
+# Print variables
+print("Name:", name)
+print("Age:", age)
+print("Weight:", weight)
+print("Is Student:", is_student)
+
+# Task 3: Type Checking
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(is_student))
